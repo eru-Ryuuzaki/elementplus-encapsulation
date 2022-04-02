ee9ebb951df1749fc415173f3e30fedc67705e8f
Ryuuzaki <1228950291@qq.com>
2022-04-02 12:06:34 +0800

    feat:menu组件封装

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+import { App } from "vue";
+import menu from "./menu";
+
+const components = [menu];
+
+export default {
+  install(app: App) {
+    components.map((item) => {
+      app.use(item);
+    });
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as Icons from "@element-plus/icons";
 import { toLine } from "./utils";
+import mUI from "./components";
 
 const app = createApp(App);
 
@@ -14,5 +15,5 @@ for (let i in Icons) {
   app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
 }
 
-app.use(router).use(ElementPlus);
+app.use(router).use(ElementPlus).use(mUI);
 app.mount("#app");
