603725ef680738d2d70179f292eed82502e050f3
Ryuuzaki <1228950291@qq.com>
2022-04-03 12:03:47 +0800

    feat:通知组件+通知菜单显示+list组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,9 @@ import menu from "./menu";
 import chooseIcon from "./chooseIcon";
 import chooseArea from "./chooseArea";
 import trend from "./trend";
-const components = [menu, chooseIcon, chooseArea, trend];
+import notification from "./notification";
+import list from "./list";
+const components = [menu, chooseIcon, chooseArea, trend, notification, list];
 
 export default {
   install(app: App) {
