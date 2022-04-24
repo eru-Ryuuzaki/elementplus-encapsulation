e69180038cda6535638ab02e3ce21637d1c627e7
eru-Ryuuzaki <71691374+eru-Ryuuzaki@users.noreply.github.com>
2022-04-24 10:07:00 +0000

    Deploying to gh-pages from  @ 43c71784453c47f5caf2937276d01b8171a18e98 🚀

---
+++ assets/index.5921b9f4.js
@@ -0,0 +1,1 @@
+import{d as a,a as s,o as c,f as r,u as t}from"./index.6b6dfc35.js";const m=a({setup(_){let o=e=>{console.log(e)};return(e,l)=>{const n=s("m-choose-area");return c(),r(n,{onChange:t(o)},null,8,["onChange"])}}});export{m as default};
